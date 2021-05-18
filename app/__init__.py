ae04f14781714e2e9f51ba3e52b3ce2fb12113b0
Brandon Ward <brandonward.gk@gmail.com>
2021-05-18 12:02:26 -0600

    add CORS app

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from config import Config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 
 from config import Config
 
 app = Flask(__name__)
 app. config.from_object(Config)
+CORS(app, supports_credentials=True)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 ma = Marshmallow(app)
