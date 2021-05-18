06a7bfab4016751d94d6a3229df297be92b88af5
Brandon Ward <brandonward.gk@gmail.com>
2021-05-17 19:03:29 -0600

    add createing an item and getting all items routes

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,16 @@ from app.schemas import item_schema, items_schema
 @app.route('/')
 def hello_world():
     return "Hello World!!!!!!!!!!!!!"
+
+@app.route('/create-item', methods=['POST'])
+def create_new_item():
+    item = request.json['item']
+    completed = False
+    db.session.add(item, completed)
+    db.session.commit()
+    return item.schema.jsonify(item)
+
+@app.route('/item/all', methods=['GET'])
+def get_all_items():
+    all_items = Item.query.all()
+    return jsonify(items_schema.dump(all_items))
